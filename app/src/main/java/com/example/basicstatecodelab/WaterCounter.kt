d9d3a4df79175e89fa7ab3ed56b62f4ba0bd77f7
UrielEsme-Nun <s23120174@alumnos.itsur.edu.mx>
2026-09-24 08:50:43 -0600

    Implementación de WellnessTasksList y ViewModel

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -7,26 +7,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable // Paso 11: Para persistir datos
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-// Paso 9: Composable con estado (Stateful)
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        // Paso 11: Se cambia remember por rememberSaveable
-        var count by rememberSaveable { mutableStateOf(0) }
-
-        // Llamada al componente sin estado
-        StatelessCounter(count, { count++ }, modifier)
-    }
-}
-
-// Paso 9: Composable sin estado (Stateless)
-@Composable
-fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
             Text("Has bebido $count vasos de agua.")
@@ -39,4 +30,10 @@ fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = M
             Text("Agregar vaso")
         }
     }
+}
+
+@Composable
+fun StatefulWaterCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count = count, onIncrement = { count++ }, modifier = modifier)
 }
